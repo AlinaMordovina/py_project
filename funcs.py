cbbb1eb7d4edb175a353705381daeeab1e40d877
Alina Mordovina <alina182@yandex.ru>
2023-10-13 23:44:05 +0400

    Add function get_difference_numbers

--- funcs.py
+++ funcs.py
@@ -1,3 +1,8 @@
 def get_summary_numbers(number_1, number_2):
     summary = number_1 + number_2
     return summary
+
+
+def get_difference_numbers(number_1, number_2):
+    difference = number_1 - number_2
+    return difference
